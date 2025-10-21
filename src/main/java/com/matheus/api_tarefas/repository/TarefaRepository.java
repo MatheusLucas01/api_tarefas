4640a9923b45456f7436a2d698e492fdc3694676
MatheusLucas01 <matheuslucasdesouza22@gmail.com>
2025-10-20 21:34:54 -0300

    feat: Implementa arquivo de Interface, que gerencia métodos

--- src/main/java/com/matheus/api_tarefas/repository/TarefaRepository.java
+++ src/main/java/com/matheus/api_tarefas/repository/TarefaRepository.java
@@ -1,4 +1,12 @@
 package com.matheus.api_tarefas.repository;
 
-public interface TarefaRepository {
+import com.matheus.api_tarefas.model.Tarefa;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+// Arquivo que instancia os métodos, gerenciamento de métodos!
+@Repository
+public interface TarefaRepository extends JpaRepository<Tarefa, Long> {
+
+
 }
